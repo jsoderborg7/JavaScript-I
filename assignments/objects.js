0063424cd7b99aaaf1c9fb3c4f2438c7b6138d27
Jenn Soderborg <jsoderborg7@gmail.com>
2019-07-22 16:09:38 -0600

    fixed objects stretch goal

--- assignments/objects.js
+++ assignments/objects.js
@@ -94,34 +94,34 @@ const parent = {
   "age": 70,
   "speak": function(){
     return this.name;
-  }
-}
+  },
 
-const child = {
-  "name": "George",
-  "age": 50,
-  "speak": function(){
-    return this.name;
-  }
-}
+  child: {
+    "name": "George",
+    "age": 50,
+    "speak": function(){
+      return this.name;
+    },
 
-const grandchild = {
-  "name": "Sam",
-  "age": 30,
-  "speak": function(){
-    return this.name;
+    grandchild: {
+      "name": "Sam",
+      "age": 30,
+      "speak": function(){
+        return this.name;
+      }
+    }
   }
 }
 
 // Log the parent object's name
 console.log (parent.name);
 // Log the child's age
-console.log (child.age);
+console.log (parent.child.age);
 // Log the name and age of the grandchild
-console.log (grandchild.name, grandchild.age);
+console.log (parent.child.grandchild.name, parent.child.grandchild.age);
 // Have the parent speak
 console.log (parent.speak());
 // Have the child speak
-console.log (child.speak());
+console.log (parent.child.speak());
 // Have the grandchild speak
-console.log (grandchild.speak());
+console.log (parent.child.grandchild.speak());
